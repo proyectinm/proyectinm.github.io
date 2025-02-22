090bfa722efbe5d9060e76f61497fca7711edac2
JeanDelahoz <rjeanc1to07@gmail.com>
2025-02-22 14:00:47 -0500

    funcionamiento a la pagina

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+import hamburgerMenu from "./hamburger.js";
+
+//* Comportamiento del scroll 
 document.addEventListener("click", (e) => {
   if (e.target.matches("a")) {
     window.scrollTo({
@@ -5,4 +8,8 @@ document.addEventListener("click", (e) => {
       bottom: 0
     })
   }
+})
+
+document.addEventListener("DOMContentLoaded", (e) => {
+  hamburgerMenu(".panel-btn", ".panel", ".menu a");
 })
